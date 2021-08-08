00f5cb4b177f8065d8c47e2c463a9364ffa83c8c
Radu <raadned@users.noreply.github.com>
2021-08-08 13:07:51 +0100

    print out temperature record information

--- client.py
+++ client.py
@@ -7,12 +7,12 @@ import temperature_pb2_grpc
 
 def send_temperature_entry(stub, temperatureMeasurement):
     temperature_entry = stub.TemperatureMeasurementStats(temperatureMeasurement)
-    print("[recording = %s, persisted = %s]" % (temperature_entry.recording, temperature_entry.recording))
+    print("[recording = %s, persisted = %s]" % (temperature_entry.recording, temperature_entry.persisted))
 
 
 
 def get_temperature_entry(stub):
-    send_temperature_entry(stub, temperature_pb2.TemperatureMeasurement(temperatureRecording=100))
+    send_temperature_entry(stub, temperature_pb2.TemperatureMeasurement(temperatureRecording=99))
 
 def run():
     with grpc.insecure_channel('localhost:50052') as channel:
