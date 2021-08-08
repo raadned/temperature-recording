ce2c9bdb5b55b94416c42d0ac397ff4303d759a8
Radu <raadned@users.noreply.github.com>
2021-08-09 00:12:44 +0100

    refactor client:
    - extract method

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import logging
 
 import grpc
 import sys
+
 sys.path.insert(0, './proto')
 from proto import temperature_pb2_grpc, temperature_pb2
 
@@ -11,14 +12,10 @@ def send_temperature_entry(stub, temperatureMeasurement):
     print("[recording = %s, persisted = %s]" % (temperature_entry.recording, temperature_entry.persisted))
 
 
-
-def get_temperature_entry(stub):
-    send_temperature_entry(stub, temperature_pb2.TemperatureMeasurement(temperatureRecording=99))
-
 def run():
     with grpc.insecure_channel('localhost:50052') as channel:
         stub = temperature_pb2_grpc.TemperatureRecordingStub(channel)
-        get_temperature_entry(stub)
+        send_temperature_entry(stub, temperature_pb2.TemperatureMeasurement(temperatureRecording=99))
 
 
 if __name__ == '__main__':
