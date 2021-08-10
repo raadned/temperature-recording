a4590c1dd5c9f6164bb7a6102c21365b9cc44319
Radu <raadned@users.noreply.github.com>
2021-08-10 22:32:30 +0100

    refactor server:
    - spacing

--- server.py
+++ server.py
@@ -2,13 +2,12 @@ from concurrent import futures
 import logging
 import grpc
 import sys
+
 sys.path.insert(0, './proto')
 from proto import temperature_pb2_grpc, temperature_pb2
 
 
-
 class TemperatureServicer(temperature_pb2_grpc.TemperatureRecordingServicer):
-    """Provides methods that implement functionality of route guide server."""
 
     def TemperatureMeasurementStats(self, recording, context):
         print('Received %s' % recording)
@@ -26,4 +25,3 @@ def serve():
 if __name__ == '__main__':
     logging.basicConfig()
     serve()
-
